0908b7e71345672b5a8a8ee2459e8cfcb7417709
RogerioTostes <40499106+RogerioTostes@users.noreply.github.com>
2018-06-26 19:26:04 -0300

    Add files via upload

---
+++ C#MiniMax/tic_tac_toe/AI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace tic_tac_toe
+{
+  class AI
+  {
+    public static Space GetBestMove(GameBoard gb, Player p)
+    {
+      Space? bestSpace = null;
+      List<Space> openSpaces = gb.OpenSquares;
+      GameBoard newBoard;
+
+      for (int i = 0; i < openSpaces.Count; i++)
+      {
+        newBoard = gb.Clone();
+        Space newSpace = openSpaces[i];
+
+        newBoard[newSpace.X, newSpace.Y] = p;
+
+        if (newBoard.Winner == Player.Open && newBoard.OpenSquares.Count > 0)
+        {
+          Space tempMove = GetBestMove(newBoard, ((Player)(-(int)p)));  
+          newSpace.Rank = tempMove.Rank;
+        }
+        else
+        {
+          if (newBoard.Winner == Player.Open)
+            newSpace.Rank = 0;
+          else if (newBoard.Winner == Player.X)
+            newSpace.Rank = -1;
+          else if (newBoard.Winner == Player.O)
+            newSpace.Rank = 1;
+        }
+
+        
+        if (bestSpace == null ||
+           (p == Player.X && newSpace.Rank < ((Space)bestSpace).Rank) ||
+           (p == Player.O && newSpace.Rank > ((Space)bestSpace).Rank))
+        {
+          bestSpace = newSpace;
+        }
+      }
+
+      return (Space)bestSpace;
+    }
+  }
+}
---
+++ C#MiniMax/tic_tac_toe/GameBoard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace tic_tac_toe
+{
+  public enum Player
+  {
+    X = 1,
+    O = -1,
+    Open = 0,
+  }
+
+  public abstract class GameBoard
+  {
+
+    public Player[,] squares;
+
+   
+    public abstract Player this[int x, int y] { get; set; }
+
+  
+    public abstract bool IsFull { get; }
+
+
+    public abstract int Size { get; }
+
+   
+    public abstract List<Space> OpenSquares { get; }
+
+   
+    public abstract Player Winner { get; }
+
+
+    public abstract GameBoard Clone();
+  }
+
+ 
+  public struct Space
+  {
+    public int X;
+    public int Y;
+    public double Rank;
+
+    public Space(int x, int y)
+    {
+      this.X = x;
+      this.Y = y;
+      Rank = 0;
+    }
+  }
+}
